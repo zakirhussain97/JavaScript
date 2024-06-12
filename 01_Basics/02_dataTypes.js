332ad053be3e2f88ed10a9cc2c3d9a831c3eaee1
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-12 18:54:43 +0000

    DataTypes in JS

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let name = "Zakir"
+let age = 30
+let isLoggedIn = true
+
+//Premitive Data Types//
+
+//string
+//number
+//bigint
+//boolean
+//symbol
+//null
+//undefined
+
+//Non Premitive Data Types
+
+//objects
+//array
+//function
