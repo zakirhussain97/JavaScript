f035a659ab648f4ffb82ff4a3d01a195a9b6e1ae
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-13 08:36:07 +0000

    Converstion Operations in JS

---
+++ 01_Basics/03_converstionOperations.js
@@ -0,0 +1,50 @@
+let score = "33"
+
+//console.log(typeof score)//
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+//"33" = 33
+//"33abc" = NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+//1 = true, 0 = false
+// "" = false
+//"Zak" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//*************Operations **************/
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
