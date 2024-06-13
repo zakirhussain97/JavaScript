4a1d66050b3d5dc8b31a48af73580024896c4cf5
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-13 13:37:13 +0000

    Strings in JS

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "Zakir "
+const repoCount = 30
+
+// console.log(name + repoCount + " Parrey");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Godofwar')
+
+// console.log(gameName[3])
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0, 5)
+// console.log(newString);
+
+const anotherString = gameName.slice(1, 5)
+// console.log(anotherString);
+
+const newStringOne = "      Rule  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
