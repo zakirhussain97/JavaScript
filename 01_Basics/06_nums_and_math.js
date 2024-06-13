3b0b018a30c186ba4fbe2b9de2a3b85354263d93
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-13 15:28:37 +0000

    Numbers and Math in JS

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+const score = 400;
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+ 
+// console.log(balance.toFixed(1))
+
+const otherNumber = 123.9876
+// console.log(otherNumber.toPrecision(4));
+
+const anotherNumber = 100000
+// console.log(anotherNumber.toLocaleString('en-IN'));
+
+/****************Maths************** */ 
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(6.9));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
