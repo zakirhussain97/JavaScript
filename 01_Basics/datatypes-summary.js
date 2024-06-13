7dc169fd47ccf475ddc7c263b14e4b877b32da4f
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-13 11:18:52 +0000

    Datatype Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,45 @@
+// Primitive DataTypes
+// 7 types
+/*
+String
+Number
+Boolean 
+Null
+Undefined
+Symbol
+*/
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = true
+const temp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 3245678899065n
+
+//Non Premitive Datatypes
+
+/*
+Array
+Object
+Functions
+*/
+
+const heroes = ["Spiderman", "Thor", "Ironman"]
+
+let obj = {
+    name: "Zakk",
+    age: 30
+}
+
+const myFunction = function() {
+    console.log("Hello World")
+}
+
+console.log(typeof heroes
+)
