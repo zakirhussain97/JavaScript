be1f281a4b417cc7d3322e430c9d313a19f51b0b
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-14 05:18:10 +0000

    Dates and Time in JS

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,17 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 0, 25)
+// let myCreatedDate = new Date(2024, 0, 15, 6, 7)
+let myCreatedDate = new Date("2024-04-21")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime());
