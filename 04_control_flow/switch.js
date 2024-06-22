4b3550ae4b433ec139cd5e8774741fc8821ace9d
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-22 19:24:26 +0000

    Switch in JS

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+
+        case 2:
+        console.log("Feb");
+        break;
+
+        case 3:
+        console.log("Mar");
+        break;
+
+        case 3:
+            console.log("Aprl");
+            break;    
+    default:
+        console.log("Default");
+        break;
+}
