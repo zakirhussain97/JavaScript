f90bdcdd15fe55ffdb47a3d20183477a746b23e8
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-06-22 20:00:44 +0000

    Truthy in JS

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,14 @@
+const userEmail = "z@zakir.ai"
+
+if(userEmail) {
+    console.log("Got User Email");
+} else {
+    console.log("Dont have user email");
+}
+
+//falsy value
+
+// false, 0, -0, BigInt 0n, null, "", undefined, NaN
+
+//truthy value
+//"0", "false", " ", [], {}, function() {}
