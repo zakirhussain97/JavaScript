0cd2565601c1a6991fa49286b2cffec24a8c6971
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2024-07-01 19:16:01 +0000

    do-while-loop in JS

---
+++ 05_Loops/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while (index <= 10) {
+  console.log(`Value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArr = ["superman", "batman", "thor"];
+let arr = 0;
+
+while (arr < myArr.length) {
+  console.log(`Value is ${myArr[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 11;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
